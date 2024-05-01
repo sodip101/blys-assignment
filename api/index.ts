2def2d151efa45646a886143ba33c8d50df74f4a
Sodip Thapa <sodip@appboxtech.com>
2024-05-01 21:32:12 +0545

    more changes for vercel

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 
-import router from "./routes";
+import router from "../src/routes";
 
 dotenv.config({ path: "./.env" });
 
@@ -14,3 +14,5 @@ app.use("/", router);
 app.listen(port, () => {
   console.log(`Server running at: https://localhost:${port}`);
 });
+
+export default app;
